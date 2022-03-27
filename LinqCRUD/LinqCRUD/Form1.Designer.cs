599d8329317a6c8070b79bee2eb85035d4ef7518
Jeet345 <67771941+Jeet345@users.noreply.github.com>
2022-03-27 16:28:59 +0530

    Add files via upload

---
+++ LinqCRUD/LinqCRUD/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace LinqCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.categoryTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.priceTextbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.qtyTextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.insertBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.idTextbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(509, 53);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(491, 344);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ProductName : ";
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextbox.Location = new System.Drawing.Point(262, 78);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(182, 22);
+            this.nameTextbox.TabIndex = 2;
+            // 
+            // categoryTextbox
+            // 
+            this.categoryTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoryTextbox.Location = new System.Drawing.Point(262, 123);
+            this.categoryTextbox.Name = "categoryTextbox";
+            this.categoryTextbox.Size = new System.Drawing.Size(182, 22);
+            this.categoryTextbox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Product Category : ";
+            // 
+            // priceTextbox
+            // 
+            this.priceTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priceTextbox.Location = new System.Drawing.Point(262, 175);
+            this.priceTextbox.Name = "priceTextbox";
+            this.priceTextbox.Size = new System.Drawing.Size(182, 22);
+            this.priceTextbox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Product Price : ";
+            // 
+            // qtyTextbox
+            // 
+            this.qtyTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.qtyTextbox.Location = new System.Drawing.Point(262, 230);
+            this.qtyTextbox.Name = "qtyTextbox";
+            this.qtyTextbox.Size = new System.Drawing.Size(182, 22);
+            this.qtyTextbox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(93, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Product Qty : ";
+            // 
+            // insertBtn
+            // 
+            this.insertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertBtn.Location = new System.Drawing.Point(97, 321);
+            this.insertBtn.Name = "insertBtn";
+            this.insertBtn.Size = new System.Drawing.Size(88, 37);
+            this.insertBtn.TabIndex = 9;
+            this.insertBtn.Text = "INSERT";
+            this.insertBtn.UseVisualStyleBackColor = true;
+            this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateBtn.Location = new System.Drawing.Point(223, 321);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(88, 37);
+            this.updateBtn.TabIndex = 10;
+            this.updateBtn.Text = "UPDATE";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteBtn.Location = new System.Drawing.Point(356, 321);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(88, 37);
+            this.deleteBtn.TabIndex = 11;
+            this.deleteBtn.Text = "DELETE";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // idTextbox
+            // 
+            this.idTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idTextbox.Location = new System.Drawing.Point(262, 28);
+            this.idTextbox.Name = "idTextbox";
+            this.idTextbox.Size = new System.Drawing.Size(182, 22);
+            this.idTextbox.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 526);
+            this.Controls.Add(this.idTextbox);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.insertBtn);
+            this.Controls.Add(this.qtyTextbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.priceTextbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.categoryTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameTextbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.TextBox nameTextbox;
+        internal System.Windows.Forms.TextBox categoryTextbox;
+        internal System.Windows.Forms.TextBox priceTextbox;
+        internal System.Windows.Forms.TextBox qtyTextbox;
+        internal System.Windows.Forms.Button insertBtn;
+        internal System.Windows.Forms.Button updateBtn;
+        internal System.Windows.Forms.Button deleteBtn;
+        internal System.Windows.Forms.TextBox idTextbox;
+    }
+}
+
---
+++ LinqCRUD/LinqCRUD/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqCRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        StoreDBDataContext dc = new StoreDBDataContext();
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            idTextbox.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            nameTextbox.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            categoryTextbox.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            priceTextbox.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            qtyTextbox.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        public void showData()
+        {
+
+            StoreDBDataContext dc = new StoreDBDataContext();
+            dataGridView1.DataSource = dc.Products;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            showData();
+        }
+
+        public void clearData()
+        {
+            idTextbox.Text = "";
+            nameTextbox.Text = "";
+            priceTextbox.Text = "";
+            qtyTextbox.Text = "";
+            categoryTextbox.Text = "";
+        }
+
+        private void insertBtn_Click(object sender, EventArgs e)
+        {
+
+            Product product = new Product();
+            product.ProductName = nameTextbox.Text;
+            product.ProductCategory =  Convert.ToInt32(categoryTextbox.Text);
+            product.Price = Convert.ToInt32(priceTextbox.Text);
+            product.Qty = Convert.ToInt32(qtyTextbox.Text);
+
+            StoreDBDataContext dc = new StoreDBDataContext();
+
+            dc.Products.InsertOnSubmit(product);
+            dc.SubmitChanges();
+
+            showData();
+
+            MessageBox.Show("Record Inserted");
+
+            clearData();
+
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            
+
+            Product product = dc.Products.SingleOrDefault(P => P.ProductId == int.Parse(idTextbox.Text));
+            product.ProductName = nameTextbox.Text;
+            product.ProductCategory = Convert.ToInt32(categoryTextbox.Text);
+            product.Price = Convert.ToInt32(priceTextbox.Text);
+            product.Qty = Convert.ToInt32(qtyTextbox.Text);
+
+            dc.SubmitChanges();
+
+            showData();
+
+            MessageBox.Show("Record Updated Successfully");
+
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+
+            StoreDBDataContext dc = new StoreDBDataContext();
+
+            Product product = dc.Products.SingleOrDefault(P => P.ProductId == int.Parse(idTextbox.Text));
+
+            dc.Products.DeleteOnSubmit(product);
+            dc.SubmitChanges();
+
+            showData();
+            clearData();
+
+            MessageBox.Show("Record Deleted Successfully");
+            
+        }
+    }
+}
