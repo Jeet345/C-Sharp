92fdba0ccf0c042feeaae43b850a31754bf3142b
Jeet345 <67771941+Jeet345@users.noreply.github.com>
2022-04-02 17:23:43 +0530

    Add files via upload

---
+++ JobPortalAppCS/JobPortalAppCS/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿
+namespace JobPortalAppCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.showAllBtn = new System.Windows.Forms.Button();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCompany = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupName = new System.Windows.Forms.ComboBox();
+            this.dBJobPortalDataSet = new JobPortalAppCS.DBJobPortalDataSet();
+            this.groupBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groupTableAdapter = new JobPortalAppCS.DBJobPortalDataSetTableAdapters.GroupTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBJobPortalDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showAllBtn
+            // 
+            this.showAllBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.showAllBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.showAllBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showAllBtn.Location = new System.Drawing.Point(1062, 109);
+            this.showAllBtn.Name = "showAllBtn";
+            this.showAllBtn.Size = new System.Drawing.Size(132, 32);
+            this.showAllBtn.TabIndex = 37;
+            this.showAllBtn.Text = "SHOW ALL";
+            this.showAllBtn.UseVisualStyleBackColor = false;
+            this.showAllBtn.Click += new System.EventHandler(this.showAllBtn_Click);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.searchBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.searchBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBtn.Location = new System.Drawing.Point(1088, 372);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(106, 32);
+            this.searchBtn.TabIndex = 36;
+            this.searchBtn.Text = "SEARCH";
+            this.searchBtn.UseVisualStyleBackColor = false;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(545, 378);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(237, 20);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Employer Or Company Name";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.Location = new System.Drawing.Point(808, 376);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(246, 24);
+            this.textBoxSearch.TabIndex = 34;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxId.Location = new System.Drawing.Point(242, 94);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(191, 24);
+            this.textBoxId.TabIndex = 33;
+            this.textBoxId.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(204, 403);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 32);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.deleteBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.deleteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteBtn.Location = new System.Drawing.Point(332, 403);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(88, 32);
+            this.deleteBtn.TabIndex = 31;
+            this.deleteBtn.Text = "DELETE";
+            this.deleteBtn.UseVisualStyleBackColor = false;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // addBtn
+            // 
+            this.addBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.addBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBtn.Location = new System.Drawing.Point(76, 403);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(88, 32);
+            this.addBtn.TabIndex = 30;
+            this.addBtn.Text = "INSERT";
+            this.addBtn.UseVisualStyleBackColor = false;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCity.Location = new System.Drawing.Point(242, 285);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(191, 24);
+            this.textBoxCity.TabIndex = 29;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(72, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Enter City : ";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEmail.Location = new System.Drawing.Point(242, 236);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(191, 24);
+            this.textBoxEmail.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(72, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 20);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Enter Email : ";
+            // 
+            // textBoxCompany
+            // 
+            this.textBoxCompany.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCompany.Location = new System.Drawing.Point(242, 191);
+            this.textBoxCompany.Name = "textBoxCompany";
+            this.textBoxCompany.Size = new System.Drawing.Size(191, 24);
+            this.textBoxCompany.TabIndex = 25;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 20);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Company Name :  ";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxName.Location = new System.Drawing.Point(242, 145);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(191, 24);
+            this.textBoxName.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Enter Name : ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dataGridView1.Location = new System.Drawing.Point(549, 147);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(645, 198);
+            this.dataGridView1.TabIndex = 21;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(416, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(395, 39);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "Employer Management";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(72, 335);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 20);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "User Type : ";
+            // 
+            // groupName
+            // 
+            this.groupName.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.groupBindingSource, "GroupId", true));
+            this.groupName.DataSource = this.groupBindingSource;
+            this.groupName.DisplayMember = "GroupName";
+            this.groupName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupName.FormattingEnabled = true;
+            this.groupName.Location = new System.Drawing.Point(242, 333);
+            this.groupName.Name = "groupName";
+            this.groupName.Size = new System.Drawing.Size(191, 26);
+            this.groupName.TabIndex = 40;
+            this.groupName.Text = "Select User Type";
+            this.groupName.ValueMember = "GroupId";
+            // 
+            // dBJobPortalDataSet
+            // 
+            this.dBJobPortalDataSet.DataSetName = "DBJobPortalDataSet";
+            this.dBJobPortalDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // groupBindingSource
+            // 
+            this.groupBindingSource.DataMember = "Group";
+            this.groupBindingSource.DataSource = this.dBJobPortalDataSet;
+            // 
+            // groupTableAdapter
+            // 
+            this.groupTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1242, 544);
+            this.Controls.Add(this.groupName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.showAllBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxCompany);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBJobPortalDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button showAllBtn;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCompany;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox groupName;
+        private DBJobPortalDataSet dBJobPortalDataSet;
+        private System.Windows.Forms.BindingSource groupBindingSource;
+        private DBJobPortalDataSetTableAdapters.GroupTableAdapter groupTableAdapter;
+    }
+}
+
---
+++ JobPortalAppCS/JobPortalAppCS/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JobPortalAppCS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DBJobPortalEntities dBJobPortalEntities = new DBJobPortalEntities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dBJobPortalDataSet.Group' table. You can move, or remove it, as needed.
+            this.groupTableAdapter.Fill(this.dBJobPortalDataSet.Group);
+            showData();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(textBoxName.Text) ||
+                string.IsNullOrEmpty(textBoxCompany.Text) ||
+                string.IsNullOrEmpty(textBoxEmail.Text) ||
+                string.IsNullOrEmpty(textBoxCity.Text))
+            {
+                MessageBox.Show("Please Fill All Textbox");
+            }
+            else
+            {
+                Employer emp = new Employer();
+                emp.EmployerName = textBoxName.Text;
+                emp.CompanyName = textBoxCompany.Text;
+                emp.Email = textBoxEmail.Text;
+                emp.City = textBoxCity.Text;
+                emp.GroupId = Convert.ToInt32(groupName.SelectedValue);
+
+                dBJobPortalEntities.Employers.Add(emp);
+                dBJobPortalEntities.SaveChanges();
+
+                showData();
+
+                MessageBox.Show("Record Inserted Successfully");
+
+                clearData();
+            }
+
+        }
+
+        public void showData()
+        {
+            dataGridView1.DataSource = dBJobPortalEntities.Employers.Select(e => new
+            {
+                e.EmployerId,
+                e.EmployerName,
+                e.Email,
+                e.CompanyName,
+                e.City,
+                e.Group.GroupName
+            }).ToList();
+        }
+
+        public void clearData()
+        {
+            textBoxCity.Text = "";
+            textBoxCompany.Text = "";
+            textBoxEmail.Text = "";
+            textBoxName.Text = "";
+            textBoxId.Text = "";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxId.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textBoxName.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            textBoxEmail.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            textBoxCompany.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            textBoxCity.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+            groupName.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(textBoxId.Text) ||
+                string.IsNullOrEmpty(textBoxName.Text) ||
+               string.IsNullOrEmpty(textBoxCompany.Text) ||
+               string.IsNullOrEmpty(textBoxEmail.Text) ||
+               string.IsNullOrEmpty(textBoxCity.Text))
+            {
+                MessageBox.Show("Please Fill All Textbox");
+            }
+            else
+            {
+
+                int empId = Convert.ToInt32(textBoxId.Text);
+
+                Employer emp = dBJobPortalEntities.Employers.Find(empId);
+                emp.EmployerName = textBoxName.Text;
+                emp.CompanyName = textBoxCompany.Text;
+                emp.Email = textBoxEmail.Text;
+                emp.City = textBoxCity.Text;
+                emp.GroupId = Convert.ToInt32(groupName.SelectedValue);
+
+                dBJobPortalEntities.SaveChanges();
+
+                showData();
+
+                MessageBox.Show("Record Updated Successfully");
+
+                clearData();
+
+            }
+
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(textBoxId.Text))
+            {
+                MessageBox.Show("Something Wan't Wrong");
+            }
+            else
+            {
+
+                int empId = Convert.ToInt32(textBoxId.Text);
+
+                Employer emp = dBJobPortalEntities.Employers.Find(empId);
+
+                dBJobPortalEntities.Employers.Remove(emp);
+                dBJobPortalEntities.SaveChanges();
+
+                showData();
+
+                MessageBox.Show("Record Deleted Successfully");
+
+                clearData();
+            }
+
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(textBoxSearch.Text))
+            {
+                MessageBox.Show("Search TextBox Can't Be Null");
+            }
+            else
+            {
+                string name = textBoxSearch.Text;
+                dataGridView1.DataSource = (from emp in dBJobPortalEntities.Employers
+                                            select new
+                                            {
+                                                emp.EmployerId,
+                                                emp.EmployerName,
+                                                emp.Email,
+                                                emp.CompanyName,
+                                                emp.City,
+                                                emp.Group.GroupName
+                                            }).Where(emp => emp.EmployerName.Contains(name)
+                                            || emp.CompanyName.Contains(name))
+                                            .ToList();
+
+
+            }
+
+        }
+
+        private void showAllBtn_Click(object sender, EventArgs e)
+        {
+            showData();
+        }
+    }
+}
